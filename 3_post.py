35bf53e5c6c3353096f80a3d571d1228acd1dbf3
lrnq <26392224+lrnq@users.noreply.github.com>
2024-12-03 07:06:59 +0100

    add post sol

---
+++ 3_post.py
@@ -0,0 +1,19 @@
+import re 
+from aocd import get_data
+
+data = "do()" + get_data()
+p = r"mul\((\d+),(\d+)\)"
+for part2 in [0, 1]:
+    ans = 0
+    flag = 0
+    for i, c in enumerate(data):
+        if data[i:].startswith("don't()"):
+            flag = 0
+        elif data[i:].startswith("do()"):
+            flag = 1
+        elif flag or not part2:
+            match = re.match(p, data[i:])
+            if match is not None:
+                a, b = map(int, match.groups())
+                ans += a*b
+    print(f"Part {part2 + 1}:", ans)
