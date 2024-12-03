1348c0b6af3be677b665a4015188fa9817806878
lrnq <26392224+lrnq@users.noreply.github.com>
2024-12-03 06:20:43 +0100

    tried to make leaderboard but the regex was too long gone

--- 3.py
+++ 3.py
@@ -2,16 +2,15 @@ import re
 from aocd import get_data
 
 data = "do()" + get_data()
-
-matches = re.findall(r"mul\((\d+),(\d+)\)", data)
+p = r"mul\((\d+),(\d+)\)"
+matches = re.findall(p, data)
 print("Part 1:", sum(int(x) * int(y) for x, y in matches))
 
-results = []
-segments = re.split(r"do\(\)", data)
-if len(segments) > 1:  
-    for segment in segments[1:]:
-        segment_before_dont = re.split(r"don't\(\)", segment)[0]
-        pattern_mul = r"mul\((\d+),(\d+)\)"
-        matches = re.findall(pattern_mul, segment_before_dont)
-        results.extend((int(x), int(y)) for x, y in matches)
-print("Part 2:", sum(int(x) * int(y) for x, y in results))
+ans = 0
+segments = data.split("do()")
+for segment in segments[1:]:
+    segment_before_dont = re.split(r"don't\(\)", segment)[0]
+    matches = re.findall(p, segment_before_dont)
+    for x, y in matches:
+        ans += int(x) * int(y)
+print("Part 2:", ans)
